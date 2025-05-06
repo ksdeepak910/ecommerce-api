7f9d3bf9e52ea802165d6f0b0c3927eefbc81e84
Deepak Sabat <deepak_sabat@epam.com>
2025-05-06 14:13:36 +0530

    second commit

---
+++ src/test/java/com/poc/ecommerce/EcommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
